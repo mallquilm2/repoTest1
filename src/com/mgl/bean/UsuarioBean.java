1247313aa55e5c0c0a0e2189cd178cf57c36f6fc
Miguel <mallquilm@gmail.com>
2017-08-20 22:54:42 -0500

    Avance login con acceso a BD Mysql

---
+++ src/com/mgl/bean/UsuarioBean.java
@@ -0,0 +1,51 @@
+package com.mgl.bean;
+
+import java.util.Date;
+
+public class UsuarioBean {
+	private String id;
+	private String name;
+	private String email;
+	private String password;
+	private Date created;
+	private Date updated;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Date getUpdated() {
+		return updated;
+	}
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+	
+	
+}
